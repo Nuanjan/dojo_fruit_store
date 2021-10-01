b3dc632420a75d49744e6d7e501258d7c1aa13ae
Nuanjan <nuanjan.schluntz@gmail.com>
2021-09-30 22:30:46 -0400

    add the form request and render checkout page.

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route('/')         
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
-def checkout():
-    print(request.form)
-    return render_template("checkout.html")
 
-@app.route('/fruits')         
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    print(request.form['strawberry'], " this strawberry qua")
+    return render_template("checkout.html", formData=request.form)
+
+
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
